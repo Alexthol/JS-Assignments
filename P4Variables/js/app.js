8294a28d32e7f69f3fe5d68c586b43c13cc41b56
Alexthol <aletho225@ihmeducation.se>
2024-09-12 11:40:02 +0200

    Update app.js

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,13 +1,18 @@
 let numberOfBottles = 10
+//Gett variabeln numberOfBottles ett värde
 const line1 = "green bottles hanging on the wall"
+//Gett variabeln line1 ett värde
 const line2 = "and if one green bottle, should accidentally fall, there will be"
+//Gett variabeln line2 ett värde
+
 
 console.log(numberOfBottles);
 console.log(line1);
 console.log(numberOfBottles);
 console.log(line1);
 console.log(line2);
-
---numberOfBottles;
+numberOfBottles -= 5;
 console.log(numberOfBottles);
+console.log(line1);
+
 
