dd1a5ca9fcd9a5cd4ae25b505d09e34493feb2ef
Alexthol <aletho225@ihmeducation.se>
2024-09-12 14:11:10 +0200

    Add Basic Operators

---
+++ P5-Operators/js/app.js
@@ -0,0 +1,21 @@
+let gold = 50;
+gold + 10;
+console.log(gold);
+
+gold = gold + 10;
+console.log(gold);
+
+gold += 10
+console.log(gold);
+
+gold *= 10;
+console.log(gold);
+
+gold /= 10
+console.log(gold);
+
+gold -= 10
+console.log(gold);
+
+gold %= 2;
+console.log(gold);
