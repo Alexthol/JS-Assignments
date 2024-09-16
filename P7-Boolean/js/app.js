edffd7138f7a5d49254ed08c6194ac2e9d84ab3a
Alexthol <aletho225@ihmeducation.se>
2024-09-16 13:06:07 +0200

    Ändring

--- P7-Boolean/js/app.js
+++ P7-Boolean/js/app.js
@@ -1,20 +1,41 @@
-//Output:You are a child: False
-//Output:You are a teenager: False
-//Output:You are an adult: True
 
-//Start with a variable
+//const birthYear = 2030;
+//const currentYear = 2024;
+//let ageOfPerson = currentYear - birthYear;
+
+//let isChild = (ageOfPerson >= 0) && (ageOfPerson <= 10);
+//let isTeenager = (ageOfPerson >= 11 && ageOfPerson <= 17);
+//let isAdult = ageOfPerson >= 18;
+
+//console.log(isChild)
+//console.log(isTeenager)
+//console.log(isAdult)
+
+
+//let playerTouchGround = false;
+//let playerHasJetPack = false;
+
+//if (playerTouchGround) {
+ // console.log("Player is on the ground");
+ // console.log("Player can jump");
+//}
+
+//else if (playerHasJetPack) {
+  //console.log("Player can Jet Away");
+//}
+
+//else {
+  //console.log("Player is NOT on the ground!");
+  //console.log("Player CANNOT jump!");
+//}
 
 
 
 
-const birthYear = 2000;
-const currentYear = 2024;
-let ageOfPerson = currentYear - birthYear;
 
-let isChild = ageOfPerson <= 10;
-let isTeenager = (ageOfPerson >= 11 && ageOfPerson <= 17);
-let isAdult = ageOfPerson >= 18;
 
-console.log(isChild)
-console.log(isTeenager)
-console.log(isAdult)
+
+
+
+
+
