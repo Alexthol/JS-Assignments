1c7f55b1dbf48b7c8e59045b93846dcb6cb912a7
Alexthol <aletho225@ihmeducation.se>
2024-09-16 10:39:45 +0200

    CreateP7Boolean

--- P4Variables/js/app.js
+++ P4Variables/js/app.js
@@ -1,19 +1,6 @@
 let numberOfBottles = 10
-//Gett variabeln numberOfBottles ett värde
-const line1 = "green bottles hanging on the wall"
-//Gett variabeln line1 ett värde
+const line1 = " green bottles hanging on the wall"
 const line2 = "and if one green bottle, should accidentally fall, there will be"
-//Gett variabeln line2 ett värde
 
 
-console.log(numberOfBottles);
-console.log(line1);
-console.log(numberOfBottles);
-console.log(line1);
-console.log(line2);
-numberOfBottles -= 5;
-numberOfBottles -= 5;
-console.log(numberOfBottles);
-console.log(line1);
-
-
+let isChild =
