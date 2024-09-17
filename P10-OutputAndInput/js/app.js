ccb6f78c951cf7629db89aa6a08da09745225c36
Alexthol <aletho225@ihmeducation.se>
2024-09-17 10:55:25 +0200

    p10

---
+++ P10-OutputAndInput/js/app.js
@@ -0,0 +1,43 @@
+//function multiplyBySelf(numberToMultiply)  {
+//return numberToMultiply * numberToMultiply;
+
+//}
+//console.log(multiplyBySelf(5));
+
+//function add(firstNumber, secondNumber) {
+ // return firstNumber + secondNumber;
+//}
+
+//console.log(add(5, 4));
+
+
+
+//function averageTwoNumbers(num1, num2) {
+  //return (num1 + num2) / 2;
+
+//}
+
+//let result = averageTwoNumbers(2, 6);
+//console.log(result);  // Output: 4
+
+
+//function nameInput(firstName, lastName){
+  //return firstName + ' ' + lastName;
+
+//}
+
+//let result = nameInput("Alexander", "Tholsson");
+//console.log("Welcome " + result + "!" );
+
+
+//function yearOfBirth(yearToday, yourAge) {
+  //return yearToday - yourAge;
+
+//}
+
+  //let result = yearOfBirth(2024, 30)
+  //console.log("Du är född år", result + "!")
+
+
+
+
