74339d374e581b141cf7e30af90bf285bc6ed2cb
Alexthol <aletho225@ihmeducation.se>
2024-09-23 13:05:44 +0200

    P11-ForLoops

---
+++ P11-ForLoops/js/app.js
@@ -0,0 +1,22 @@
+for (let i = 0; i < 13; i++) {
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
+  console.log(randomNumber);
+}
+
+
+
+for (let number = 1; number <= 1024; number *= 2) {
+  console.log(number);
+}
+
+
+for (let i = 10; i >= 0; i--) {
+  console.log(i);
+}
+
+
+
+
+
+
+
